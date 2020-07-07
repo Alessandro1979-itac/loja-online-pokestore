c49ff14e40e235699731d71bd900c106f915a24e
Alessandro <sandromuniz27@hotmail.com>
2020-07-07 15:09:49 -0400

    feat: adicionado no diretório src o arquivo index com sua estrutura de códigos

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+
+import "./index.scss";
+import App from "./statefull/App";
+import { store, persistor } from "./store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
