2c90f2b0f0a7d108bcfa74c36f9945620d2b1a45
Alessandro <sandromuniz27@hotmail.com>
2020-07-07 14:35:38 -0400

    feat: adicionado no diretório statefull o sub-diretório SearchInput e o arquivo index com sua estrutura de códigos

---
+++ src/statefull/SearchInput/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { DebounceInput } from "react-debounce-input";
+import { useDispatch } from "react-redux";
+
+import "./styles.scss";
+import { setSearchingProducts } from "../../actions";
+
+function SearchInput() {
+  const dispatch = useDispatch();
+
+  return (
+    <DebounceInput
+      type="text"
+      minLength={1}
+      className="search-input"
+      debounceTimeout={800}
+      onChange={({ target: { value } }) => dispatch(setSearchingProducts(value.toLowerCase()))}
+      placeholder="Qual carta você procura?"
+    />
+  );
+}
+
+export default SearchInput;
