def94ebbd94434900e151717f163a5003bc2cc45
Alessandro <sandromuniz27@hotmail.com>
2020-07-07 15:06:05 -0400

    feat: adicionado o diretório store e o arquivo index com sua estrutura de códigos

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createSagaMiddleware from "redux-saga";
+
+import rootSaga from "./sagas";
+import rootReducer from "../reducers";
+
+const persistConfig = {
+  key: "pokestoreStorage",
+  storage,
+  blacklist: ["products"],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
