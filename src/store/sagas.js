51bb97e00f97158028a66299ed2ff516ed8ae3b2
Alessandro <sandromuniz27@hotmail.com>
2020-07-07 15:08:16 -0400

    feat: adicionado no diretório store o arquivo sagas com sua estrutura de códigos

---
+++ src/store/sagas.js
@@ -0,0 +1,53 @@
+import { put, takeLatest, all, call } from 'redux-saga/effects';
+
+function fetchProducts() {
+  return fetch("https://api.reinaldowft.com/pokestore?limit=25&page=6")
+    .then((data) => data.json())
+    .then((data) => data.results)
+}
+
+function searchProducts(payload) {
+  return fetch("https://api.reinaldowft.com/pokestore/search", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({query: payload})
+  })
+    .then((data) => data.json())
+}
+
+function* callFetchProducts() {
+  try {
+    const products = yield call(fetchProducts)
+    yield put({ type: 'PRODUCTS_FETCH_SUCCESS', payload: products })
+  }
+  catch(error) {
+    yield put({ type: 'PRODUCTS_FETCH_ERROR', payload: error })
+  }
+}
+
+function* callSearchProducts(args) {
+  try {
+    const products = yield call(searchProducts, args.payload)
+    yield put({ type: 'PRODUCTS_FETCH_SUCCESS', payload: products })
+  }
+  catch(error) {
+    yield put({ type: 'PRODUCTS_FETCH_ERROR', payload: error })
+  }
+}
+
+export function* watchProductsFetchCall() {
+  yield takeLatest('PRODUCTS_FETCH', callFetchProducts)
+}
+
+export function* watchProductsSearchCall() {
+  yield takeLatest('SEARCHING_PRODUCTS', callSearchProducts)
+}
+
+export default function* rootSaga() {
+  yield all([
+    watchProductsFetchCall(),
+    watchProductsSearchCall()
+  ])
+}
