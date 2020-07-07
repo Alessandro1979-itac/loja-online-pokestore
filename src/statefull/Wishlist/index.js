c3923465e3162ec48b1306ebc14320c91d863d37
Alessandro <sandromuniz27@hotmail.com>
2020-07-07 14:42:39 -0400

    feat: adicionado no diretório statefull o sub-diretório Wishlist e o arquivo index com sua estrutura de códigos

---
+++ src/statefull/Wishlist/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import Drawer from "../../stateless/Drawer";
+import CardProduct from "../../statefull/CardProduct";
+import { closeWishlistDrawer } from "../../actions";
+
+function Wishlist() {
+  const { shouldShowWishlist, wishlist } = useSelector(
+    (state) => state.wishlist
+  );
+
+  return shouldShowWishlist ? (
+    <Drawer
+      title="Lista de desejos"
+      closeFunction={closeWishlistDrawer}
+    >
+      {
+        wishlist.length ?
+        wishlist.map((prod) => (
+          <CardProduct
+            wishlist
+            product={prod}
+            key={prod.id}
+            closeFunction={closeWishlistDrawer}
+          />
+        ))
+        :
+        <p className="drawer__empty">Adicione as cartas e elas aparecerão aqui! :)</p>
+      }
+    </Drawer>
+  ) : null;
+}
+
+export default Wishlist;
